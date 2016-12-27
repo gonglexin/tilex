4179fc58750c0cb30a37ead692b9780160d6eebd
Jake Worth <jake@jakeworth.com>
2016-12-27 16:57:40 -0600

    Get secrets from the environment

--- config/prod.exs
+++ config/prod.exs
@@ -14,6 +14,7 @@ use Mix.Config
 config :tilex, Tilex.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
@@ -55,7 +56,3 @@ config :logger, level: :info
 #
 #     config :tilex, Tilex.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
